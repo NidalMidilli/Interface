33548f75d5897b50a5b0299e1d84896cb8f993e2
Nidal Midilli <nidalmidilli@gmail.com>
2022-10-07 10:50:36 +0300

    interface ders

---
+++ Banka/Banka/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Banka
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ziraat ziraat = new Ziraat();
+            ziraat.yatir();
+           // ziraat.yatir();
+           Garanti garanti = new Garanti();
+           // garanti.cek();
+        }
+    }
+}
---
+++ Banka/Banka/Ziraat.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banka
+{
+    public interface IParaCekme
+    {
+        void cek();
+
+    }
+    public interface IparaYatirma
+    {
+        void yatir();
+    }
+
+    public class Ziraat : IParaCekme, IparaYatirma
+    {
+        public void cek()
+        {
+            Console.WriteLine("Bakiye Giriniz:");
+            int bakiye = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Çekilecek ücret giriniz:");
+            int cekilenmiktar = Convert.ToInt32(Console.ReadLine());
+            bakiye -= cekilenmiktar;
+            Console.WriteLine("Yeni Bakiyeniz"+bakiye);
+        }
+
+        public void yatir()
+        {
+            Console.WriteLine("Bakiye Giriniz:");
+            int bakiye = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Yatırılacak miktarı giriniz:");
+            int yatirilanmiktar = Convert.ToInt32(Console.ReadLine());
+            bakiye += yatirilanmiktar;
+            Console.WriteLine("Yeni Bakiyeniz" + bakiye);
+        }
+    }
+    public class Garanti : IParaCekme, IparaYatirma
+    {
+        public void cek()
+        {
+           
+            Console.WriteLine("Bakiye Giriniz:");
+            int bakiye = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Çekilecek ücret giriniz: Banka 2 tl işlem ücreti uygulayacaktır.");
+            int cekilenmiktar = Convert.ToInt32(Console.ReadLine());
+            bakiye = bakiye - cekilenmiktar;
+            Console.WriteLine("Yeni Bakiyeniz" + (bakiye - 2));
+        }
+
+        public void yatir()
+        {
+            Console.WriteLine("Bakiye Giriniz:");
+            int bakiye = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Yatırılacak miktarı giriniz: Banka 2 tl işlem ücreti uygulayacaktır.");
+            int yatirilanmiktar = Convert.ToInt32(Console.ReadLine());
+            bakiye += yatirilanmiktar;
+            Console.WriteLine("Yeni Bakiyeniz" + (bakiye-2));
+        }
+    }
+
+}
